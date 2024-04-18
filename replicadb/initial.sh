ef645315396b70d19649c1174631793ba351e788
SamoProgrammer <samo092813463@gmail.com>
2024-04-18 14:22:03 +0330

    master port added

--- replicadb/initial.sh
+++ replicadb/initial.sh
@@ -4,7 +4,7 @@
 # source ../.env
 
 # Connect slave to master.
-result=$(docker exec $MYSQL_MASTER_DATABASE mariadb -u root --password=$MYSQL_MASTER_ROOT_PASSWORD --execute="show master status;")
+result=$(docker exec mariadb_master mariadb -u root --password=$MYSQL_MASTER_ROOT_PASSWORD --execute="show master status;")
 log=$(echo $result | awk '{print $5}')
 position=$(echo $result | awk '{print $6}')
 
@@ -12,7 +12,8 @@ docker exec mariadb_replica \
     mariadb -u root --password=$MYSQL_REPLICA_ROOT_PASSWORD \
     --execute="stop slave;\
    reset slave;\
-   CHANGE MASTER TO MASTER_HOST='$MYSQL_MASTER_DATABASE', MASTER_USER='$REPLICATION_USER_NAME', \
-   MASTER_PASSWORD='$REPLICATION_USER_PASSWORD', MASTER_LOG_FILE='$log', MASTER_LOG_POS=$position;\
+   CHANGE MASTER TO MASTER_HOST='$MASTER_DB_HOST', MASTER_USER='$REPLICATION_USER_NAME', \
+   MASTER_PASSWORD='$REPLICATION_USER_PASSWORD', MASTER_LOG_FILE='$log', MASTER_PORT=3306,
+   MASTER_CONNECT_RETRY=10 , MASTER_LOG_POS=$position;\
    start slave;\
    SHOW SLAVE STATUS\G;"
