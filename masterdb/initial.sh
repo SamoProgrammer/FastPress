62562f9c128b51efc9dc80536d39f2553a4ea179
SamoProgrammer <samo092813463@gmail.com>
2024-04-18 12:10:51 +0330

    environment variables formatting changed

--- masterdb/initial.sh
+++ masterdb/initial.sh
@@ -5,7 +5,7 @@
 
 # Create user on master database.
 docker exec mariadb_master \
-    mariadb -u root --password=$MYSQL_MASTER_ROOT_PASSWORD \
+    mariadb -u root --password=$MARIADB_MASTER_ROOT_PASSWORD \
     --execute="create user '$REPLICATION_USER_NAME'@'%' identified by '$REPLICATION_USER_PASSWORD';\
    grant replication slave on *.* to '$REPLICATION_USER_NAME'@'%';\
    flush privileges;"
--- replicadb/initial.sh
+++ replicadb/initial.sh
@@ -4,15 +4,15 @@
 # source ../.env
 
 # Connect slave to master.
-result=$(docker exec $MYSQL_MASTER_DATABASE mariadb -u root --password=$MYSQL_MASTER_ROOT_PASSWORD --execute="show master status;")
+result=$(docker exec $MARIADB_MASTER_DATABASE mariadb -u root --password=$MARIADB_MASTER_ROOT_PASSWORD --execute="show master status;")
 log=$(echo $result | awk '{print $5}')
 position=$(echo $result | awk '{print $6}')
 
 docker exec mariadb_replica \
-    mariadb -u root --password=$MYSQL_REPLICA_ROOT_PASSWORD \
+    mariadb -u root --password=$MARIADB_REPLICA_ROOT_PASSWORD \
     --execute="stop slave;\
    reset slave;\
-   CHANGE MASTER TO MASTER_HOST='$MYSQL_MASTER_DATABASE', MASTER_USER='$REPLICATION_USER_NAME', \
+   CHANGE MASTER TO MASTER_HOST='$MARIADB_MASTER_DATABASE', MASTER_USER='$REPLICATION_USER_NAME', \
    MASTER_PASSWORD='$REPLICATION_USER_PASSWORD', MASTER_LOG_FILE='$log', MASTER_LOG_POS=$position;\
    start slave;\
    SHOW SLAVE STATUS\G;"
