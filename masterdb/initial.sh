089e522ad33935cd23f79a56512762678c8be8d2
SamoProgrammer <samo092813463@gmail.com>
2024-04-18 21:18:40 +0330

    env vars updated

--- masterdb/initial.sh
+++ masterdb/initial.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # .env file variables sourced into script
-source .env
+# source .env
 
-echo "root password is : $MYSQL_MASTER_ROOT_PASSWORD"
+echo "root password is : $MASTER_ROOT_PASSWORD"
 
 # Create user on master database.
 docker exec mariadb_master \
-    mariadb -u root --password=$MYSQL_MASTER_ROOT_PASSWORD \
+    mariadb -u root --password=$MASTER_ROOT_PASSWORD \
     --execute="create user '$REPLICATION_USER_NAME'@'%' identified by '$REPLICATION_USER_PASSWORD';\
    grant replication slave on *.* to '$REPLICATION_USER_NAME'@'%';\
-   CREATE DATABASE $MYSQL_MASTER_DATABASE;\
+   CREATE DATABASE $MASTER_DATABASE;\
    flush privileges;"
--- replicadb/initial.sh
+++ replicadb/initial.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # .env file variables sourced into script
-source .env
+# source .env
 
 # Connect slave to master.
-result=$(docker exec mariadb_master mariadb -u root --password=$MYSQL_MASTER_ROOT_PASSWORD --execute="show master status;")
+result=$(docker exec mariadb_master mariadb -u root --password=$MASTER_ROOT_PASSWORD --execute="show master status;")
 log=$(echo $result | awk '{print $5}')
 position=$(echo $result | awk '{print $6}')
 
 docker exec mariadb_replica \
-    mariadb -u root --password=$MYSQL_REPLICA_ROOT_PASSWORD \
+    mariadb -u root --password=$REPLICA_ROOT_PASSWORD \
     --execute="stop slave;\
    reset slave;\
    CHANGE MASTER TO MASTER_HOST='$MASTER_DB_HOST', MASTER_USER='$REPLICATION_USER_NAME', \
