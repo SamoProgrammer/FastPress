815f6784e20ce65a88c219b36646add7ba6cd189
SamoProgrammer <samo092813463@gmail.com>
2024-04-18 12:15:43 +0330

    mysql used in environment variables

--- masterdb/initial.sh
+++ masterdb/initial.sh
@@ -3,11 +3,11 @@
 # .env file variables sourced into script
 # source ../.env
 
-echo $MARIADB_MASTER_ROOT_PASSWORD
+echo $MYSQL_MASTER_ROOT_PASSWORD
 
 # Create user on master database.
 docker exec mariadb_master \
-    mariadb -u root --password=$MARIADB_MASTER_ROOT_PASSWORD \
+    mariadb -u root --password=$MYSQL_MASTER_ROOT_PASSWORD \
     --execute="create user '$REPLICATION_USER_NAME'@'%' identified by '$REPLICATION_USER_PASSWORD';\
    grant replication slave on *.* to '$REPLICATION_USER_NAME'@'%';\
    flush privileges;"
