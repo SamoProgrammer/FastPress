98e9c2c1bb416683afbaf2111918f27a99c6167f
SamoProgrammer <samo092813463@gmail.com>
2024-04-18 14:21:24 +0330

    database created in script

--- masterdb/initial.sh
+++ masterdb/initial.sh
@@ -3,11 +3,12 @@
 # .env file variables sourced into script
 # source ../.env
 
-echo $MYSQL_MASTER_ROOT_PASSWORD
+echo "root password is : $MYSQL_MASTER_ROOT_PASSWORD"
 
 # Create user on master database.
 docker exec mariadb_master \
     mariadb -u root --password=$MYSQL_MASTER_ROOT_PASSWORD \
     --execute="create user '$REPLICATION_USER_NAME'@'%' identified by '$REPLICATION_USER_PASSWORD';\
    grant replication slave on *.* to '$REPLICATION_USER_NAME'@'%';\
+   CREATE DATABASE $MYSQL_MASTER_DATABASE;\
    flush privileges;"
