1d455b2fbdc882c433f02383e99dbcd7f5701cbc
SamoProgrammer <samo092813463@gmail.com>
2024-04-18 22:34:09 +0330

    mysql users updated

--- proxysql/initial.sh
+++ proxysql/initial.sh
@@ -5,8 +5,9 @@ source .env
 admin_credentials="admin:$PROXYSQL_ADMIN_PASSWORD"
 mariadb_master_host="$MASTER_DB_HOST"
 mariadb_replica_host="$REPLICA_DB_HOST"
-master_db_user="$DB_USER"
-master_db_password="$DB_PASSWORD"
+master_db_user="$MASTER_USER"
+master_db_password="$MASTER_PASSWORD"
+
 
 # Generate proxysql.cnf content
 cat <<EOF >proxysql.cnf
@@ -21,21 +22,21 @@ admin_variables=
 # MySQL Servers
 mysql_servers =
 (
-    { address="$mariadb_master_host", port=3306, hostgroup=10 },
-    { address="$mariadb_replica_host", port=3306, hostgroup=20 }
+    { address="$mariadb_master_host", port=3306, hostgroup=1 },
+    { address="$mariadb_replica_host", port=3306, hostgroup=2 }
 )
 
 # MySQL Users
 mysql_users =
 (
-    { username = "$db_user", password = "$db_password", default_hostgroup = 10, active = 1 },
-    { username = "$db_user", password = "$db_password", default_hostgroup = 20, active = 1 }
+    { username = "$master_db_user", password = "$master_db_password", default_hostgroup = 1, active = 1 },
+    { username = "$master_db_user", password = "$master_db_password", default_hostgroup = 2, active = 1 }
 )
 
 # MySQL Query Rules
 mysql_query_rules =
 (
-    { rule_id=1, active=1, match_pattern="^SELECT.*", destination_hostgroup=20 },
-    { rule_id=2, active=1, match_pattern="^UPDATE.*|^INSERT.*|DELETE.*", destination_hostgroup=10 }
+    { rule_id=1, active=1, match_pattern="^SELECT.*", destination_hostgroup=2 },
+    { rule_id=2, active=1, match_pattern="^UPDATE.*|^INSERT.*|DELETE.*", destination_hostgroup=1 }
 )
 EOF
