800d0b0a6cfb50e4d88a3af86da7cd2eac86286b
SamoProgrammer <samo092813463@gmail.com>
2024-04-19 11:11:31 +0330

    new alghorithm applied

--- start.bash
+++ start.bash
@@ -1,16 +1,14 @@
 source .env
 
+docker compose up -d
+sleep 6s
+source ./masterdb/initial.sh
+sleep 3s
+
+source ./replicadb/initial.sh
+sleep 2s
 source ./proxysql/initial.sh
 
 echo $MASTER_USER
 echo $mariadb_master_host
 
-docker compose up -d
-
-sleep 6s
-
-source ./masterdb/initial.sh
-
-sleep 2s
-
-source ./replicadb/initial.sh
