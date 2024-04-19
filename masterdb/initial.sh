d41dc2899651416136adc08c0658e8741807c115
SamoProgrammer <samo092813463@gmail.com>
2024-04-19 18:38:54 +0330

    lowercase queries used

--- masterdb/initial.sh
+++ masterdb/initial.sh
@@ -10,6 +10,6 @@ docker exec mariadb_master \
     mariadb -u root --password=$MASTER_ROOT_PASSWORD \
     --execute="create user '$REPLICATION_USER_NAME'@'%' identified by '$REPLICATION_USER_PASSWORD';\
    grant replication slave on *.* to '$REPLICATION_USER_NAME'@'%';\
-   CREATE DATABASE $MASTER_DATABASE;\
-   GRANT ALL PRIVILEGES ON $MASTER_DATABASE.* TO '$REPLICATION_USER_NAME'@'%';\
+   create database $MASTER_DATABASE;\
+   grant all privileges on $MASTER_DATABASE.* to '$REPLICATION_USER_NAME'@'%';\
    flush privileges;"
