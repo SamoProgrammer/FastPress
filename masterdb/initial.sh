8895cb12f24f85c0ca2e9456eb4efa4140affc10
SamoProgrammer <samo092813463@gmail.com>
2024-04-19 18:57:53 +0330

    shared user between databases

--- masterdb/initial.sh
+++ masterdb/initial.sh
@@ -10,6 +10,7 @@ docker exec mariadb_master \
     mariadb -u root --password=$MASTER_ROOT_PASSWORD \
     --execute="create user '$REPLICATION_USER_NAME'@'%' identified by '$REPLICATION_USER_PASSWORD';\
    grant replication slave on *.* to '$REPLICATION_USER_NAME'@'%';\
+   flush privileges;\
    create database $MASTER_DATABASE;\
-   grant all privileges on $MASTER_DATABASE.* to '$REPLICATION_USER_NAME'@'%';\
-   flush privileges;"
+   create user '$SHARED_USER_NAME'@'%' identified by '$SHARED_USER_PASSWORD';\
+   grant all privileges on *.* to '$SHARED_USER_NAME'@'%';"
