7b51d56fd8c66521c26894a52a49117a7783d7b4
SamoProgrammer <samo092813463@gmail.com>
2024-04-20 22:15:56 +0330

    inital script of proxysql implemented

--- start.bash
+++ start.bash
@@ -1,13 +1,15 @@
 source .env
 
+source ./proxysql/initial.sh
+
+sleep 2s
+
 docker compose up -d
+
 sleep 6s
+
 source ./masterdb/initial.sh
+
 sleep 1s
 
 source ./replicadb/initial.sh
-# sleep 2s
-# source ./proxysql/initial.sh
-
-# echo $MASTER_USER
-# echo $mariadb_master_host
