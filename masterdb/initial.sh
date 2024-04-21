a742068d03874fe516a03700930692a3e0918561
SamoProgrammer <samo092813463@gmail.com>
2024-04-21 20:41:11 +0330

    commands will run internally

--- masterdb/initial.sh
+++ masterdb/initial.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 # Create slave user and grant access on master database.
-docker exec mariadb_master \
-    mariadb -u root --password=$MASTER_ROOT_PASSWORD \
+
+mariadb -u root --password=$MASTER_ROOT_PASSWORD \
     --execute="create user if not exists '$REPLICATION_USER_NAME'@'%' identified by '$REPLICATION_USER_PASSWORD';\
    grant replication slave on *.* to '$REPLICATION_USER_NAME'@'%';\
    flush privileges;"
 
 # Creating database
-docker exec mariadb_master mariadb -u root --password=$MASTER_ROOT_PASSWORD --execute="CREATE DATABASE IF NOT EXISTS $MASTER_DATABASE;"
-
+mariadb -u root --password=$MASTER_ROOT_PASSWORD --execute="CREATE DATABASE IF NOT EXISTS $MASTER_DATABASE;"
 
 # Creating shared user between databases
-docker exec mariadb_master mariadb -u root --password=$MASTER_ROOT_PASSWORD --execute="CREATE USER IF NOT EXISTS '$SHARED_USER_NAME'@'%' IDENTIFIED BY '$SHARED_USER_PASSWORD';"
+mariadb -u root --password=$MASTER_ROOT_PASSWORD --execute="CREATE USER IF NOT EXISTS '$SHARED_USER_NAME'@'%' IDENTIFIED BY '$SHARED_USER_PASSWORD';"
 
-docker exec mariadb_master mariadb -u root --password=$MASTER_ROOT_PASSWORD --execute="GRANT ALL PRIVILEGES ON *.* TO '$SHARED_USER_NAME'@'localhost';"
+mariadb -u root --password=$MASTER_ROOT_PASSWORD --execute="GRANT ALL PRIVILEGES ON *.* TO '$SHARED_USER_NAME'@'localhost';"
 
-docker exec mariadb_master mariadb -u root --password=$MASTER_ROOT_PASSWORD --execute="GRANT ALL PRIVILEGES ON *.* TO '$SHARED_USER_NAME'@'%';"
+mariadb -u root --password=$MASTER_ROOT_PASSWORD --execute="GRANT ALL PRIVILEGES ON *.* TO '$SHARED_USER_NAME'@'%';"
--- replicadb/initial.sh
+++ replicadb/initial.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-
-result=$(docker exec mariadb_master mariadb -u root --password=$MASTER_ROOT_PASSWORD --execute="show master status;")
-
-log=$(echo $result | awk '{print $5}')
-position=$(echo $result | awk '{print $6}')
-
 # Connect slave to master.
-docker exec mariadb_replica \
-    mariadb -u root --password=$REPLICA_ROOT_PASSWORD \
+
+mariadb -u root --password=$REPLICA_ROOT_PASSWORD \
     --execute="stop slave;\
    reset slave;\
    CHANGE MASTER TO MASTER_HOST='$MASTER_DB_HOST', MASTER_USER='$REPLICATION_USER_NAME', \
-   MASTER_PASSWORD='$REPLICATION_USER_PASSWORD', MASTER_LOG_FILE='$log', MASTER_PORT=3306,
-   MASTER_CONNECT_RETRY=10 , MASTER_LOG_POS=$position;\
+   MASTER_PASSWORD='$REPLICATION_USER_PASSWORD', MASTER_PORT=3306,
+   MASTER_CONNECT_RETRY=10;\
    start slave;"
 
 # Creating shared user between databases
-docker exec mariadb_replica mariadb -u root --password=$REPLICA_ROOT_PASSWORD --execute="CREATE USER IF NOT EXISTS '$SHARED_USER_NAME'@'%' IDENTIFIED BY '$SHARED_USER_PASSWORD';"
+mariadb -u root --password=$REPLICA_ROOT_PASSWORD --execute="CREATE USER IF NOT EXISTS '$SHARED_USER_NAME'@'%' IDENTIFIED BY '$SHARED_USER_PASSWORD';"
 
-docker exec mariadb_replica mariadb -u root --password=$REPLICA_ROOT_PASSWORD --execute="GRANT ALL PRIVILEGES ON *.* TO '$SHARED_USER_NAME'@'localhost';"
+mariadb -u root --password=$REPLICA_ROOT_PASSWORD --execute="GRANT ALL PRIVILEGES ON *.* TO '$SHARED_USER_NAME'@'localhost';"
 
-docker exec mariadb_replica mariadb -u root --password=$REPLICA_ROOT_PASSWORD --execute="GRANT ALL PRIVILEGES ON *.* TO '$SHARED_USER_NAME'@'%';"
+mariadb -u root --password=$REPLICA_ROOT_PASSWORD --execute="GRANT ALL PRIVILEGES ON *.* TO '$SHARED_USER_NAME'@'%';"
